994721dcb551a1e8431d98a0a3977e1516f9dd26
mateopedehonta <mateopedehonta@gmail.com>
2024-05-13 12:42:51 -0300

    fixed

--- src/components/cart/BuyButton.tsx
+++ src/components/cart/BuyButton.tsx
@@ -1,15 +1,22 @@
 import { Product } from "@/types";
 import { Button } from "../ui/button";
 import { redirect } from "next/navigation";
-interface Prop{
-    listCart:Product[]
+interface Prop {
+  listCart: Product[];
 }
-export const BuyButton = ({listCart}:Prop) => {
-    const buyButton= ()=>{
-        const message = listCart.map(item=>`${item.quantity} ${item.title} - ${item.price}`).join(' | ')
-        // console.log(message);
-        window.open(`https://api.whatsapp.com/send?phone=542302308783&text=${message}`, '_blank');
-
-    }
+export const BuyButton = ({ listCart }: Prop) => {
+  const buyButton = () => {
+    const message = listCart
+      .map(
+        ({ quantity, tipo, description, brand, amount, price }) =>
+          `${quantity} ${tipo} ${description} ${brand} ${amount} - ${price}`,
+      )
+      .join(" | ");
+    // console.log(message);
+    window.open(
+      `https://api.whatsapp.com/send?phone=542302308783&text=${message}`,
+      "_blank",
+    );
+  };
   return <Button onClick={buyButton}> Realizar pedido</Button>;
 };
