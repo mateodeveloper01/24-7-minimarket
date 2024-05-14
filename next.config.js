eebabdfb91b2f7e7a087b7b9745cbec9b3cb028f
mateopedehonta <mateopedehonta@gmail.com>
2024-05-14 11:14:58 -0300

    image fixed

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
   },
   images: {
     // domains: ['res.cloudinary.com'],
+    unoptimized: true,
     formats:['image/webp','image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     remotePatterns: [
