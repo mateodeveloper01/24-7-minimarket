ac4401720840552499795dd23245ddc91d30ae3c
mateopedehonta <mateopedehonta@gmail.com>
2024-07-04 12:43:45 -0300

    fixed search

--- next.config.js
+++ next.config.js
@@ -17,6 +17,9 @@ const nextConfig = {
       },
     ],
   },
+  experimental:{
+    missingSuspenseWithCSRBailout:false
+  }
 };
 
 module.exports = nextConfig;
--- src/components/index.ts
+++ src/components/index.ts
@@ -14,7 +14,7 @@ export * from './home/CarouselContainer'
 export * from './order/OrderForm'
 export * from './products/ProductsCarrousel'
 export * from './pagination/PaginationComponent'
-
+export * from './search/SearchProduct'
 
 export * from "./ui/button";
 export * from "./ui/menubar";
