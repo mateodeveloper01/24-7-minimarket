ab6b8638ce0abb7577d3a3cb6783a090828263b2
mateopedehonta <mateopedehonta@gmail.com>
2024-07-11 16:56:17 -0300

    search fixed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,9 +4,9 @@ import { categories } from "@/schemas";
 export default async function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center w-full gap-10">
-      {categories.map((category: string) => (
+      {/* {categories.map((category: string) => (
         <ProductsCarrousel key={category} category={category} />
-      ))}
+      ))} */}
       <AboutMe />
     </div>
   );
